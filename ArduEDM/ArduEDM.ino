459ea63eb8132dabc91d563b0f304decb34e320c
Paul Richard <prichard@creativemachines.com>
2021-06-24 21:06:05 -0700

    Add automatic feed based on voltage

--- ArduEDM/ArduEDM.ino
+++ ArduEDM/ArduEDM.ino
@@ -4,6 +4,7 @@
 #define EN   6
 
 #define POT       A0
+#define SPARK     A1
 #define BUTT_UP   11
 #define BUTT_DN   10
 #define BUTT_TGL  12
@@ -11,6 +12,16 @@
 AccelStepper z_axis( AccelStepper::DRIVER, STEP, DIR );
 
 int potVal;
+int tgl_threshold = 49;
+int tgl_count = 0;
+bool tgl_state = 0;
+bool tgl_state_prev = 0;
+
+int spark_threshold = 30;
+int short_threshold =  5;
+float voltFactor = 3.65;
+float voltage = 0;
+unsigned long lastPrint = 0;
 
 void setup(){
   pinMode( EN, OUTPUT );
@@ -27,6 +38,40 @@ void setup(){
 }
 
 void loop(){
+  check_pot();  
+  check_tgl();
+  voltage = get_voltage();
+  if( ( millis() - lastPrint ) > 100 ){
+    Serial.println( voltage );
+  }
+  if( tgl_state ){
+    if( voltage > spark_threshold ){
+      z_axis.move( 1 );
+      z_axis.setSpeed( -100 );
+      z_axis.run();
+      //Serial.println( "Plunging" );
+    }
+    if( voltage < short_threshold ){
+      z_axis.move( -1 );
+      z_axis.setSpeed( 1000 );
+      //Serial.println( "Retracting" );
+    }
+  }
+  z_axis.run();
+}
+
+void check_tgl(){
+  tgl_count = ( !digitalRead( BUTT_TGL ) ) ? tgl_count+1 : tgl_count-1 ;
+  tgl_count = constrain( tgl_count, 0, tgl_threshold+1 );
+  if( tgl_state == tgl_state_prev && tgl_count > tgl_threshold ){
+    tgl_state = !tgl_state;
+  }
+  if( !digitalRead( BUTT_TGL ) == 0 ){
+    tgl_state_prev = tgl_state;
+  }
+}
+
+void check_pot(){
   potVal = analogRead( POT );
   if( !digitalRead( BUTT_UP ) ){
     z_axis.setSpeed( potVal );
@@ -37,6 +82,9 @@ void loop(){
   else{
     z_axis.setSpeed( 0 );
   }
-    z_axis.run();
-  
+}
+
+float get_voltage(){
+  int reading = analogRead( SPARK );
+  return reading/voltFactor; 
 }
